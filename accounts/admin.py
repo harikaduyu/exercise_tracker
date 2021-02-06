d882b2a56903301231239a6d9ca2885c8571bf1e
Harika <h.duyu@rebuy.com>
2021-02-06 13:34:15 +0100

    adding Exercise and Routine models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, Exercise, Routine
 
 
 admin.site.register(Account)
+admin.site.register(Exercise)
+admin.site.register(Routine)
 
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import timedelta
 # Create your models here.
 
 
@@ -10,7 +10,36 @@ class Account(models.Model):
     email = models.EmailField(max_length=200)
     # profile_pic = models.ImageField(
     #     default="profile1.png", null=True, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
-# class Exercise(models.Model):
+class Exercise(models.Model):
+    OPTIONS = (
+        ('Strength Training', 'Strength Training'),
+        ('Cardio', 'Cardio'),
+        ('Stretching', 'Stretching'),
+    )
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=100, choices=OPTIONS,  null=True)
+    date_created = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Routine(models.Model):
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    duration = models.DurationField(default=timedelta(minutes=25))
+    date_created = models.DateTimeField(
+        auto_now_add=True,  null=True, blank=True)
+
+    def __str__(self):
+        return self.exercise.name
