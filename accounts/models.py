d8a46c60f010226fb12ce29c324b0dd1827b1e98
Harika <h.duyu@rebuy.com>
2021-02-06 11:59:29 +0100

    add Account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200)
+    # profile_pic = models.ImageField(
+    #     default="profile1.png", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+# class Exercise(models.Model):
