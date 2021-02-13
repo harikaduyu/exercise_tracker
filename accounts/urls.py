6e5515b16bd59baa4100e6412e3b0f18d865fbb1
Harika <h.duyu@rebuy.com>
2021-02-13 16:54:55 +0100

    add exercises page and account view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('exercises/', views.exercises, name="exercises"),
+    path('routines/', views.routines, name="routines"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Exercise, Account, Routine
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'accounts/home.html')
+
+
+def exercises(request):
+    exercises = Exercise.objects.all()
+    return render(request, 'accounts/exercises.html', {'exercises': exercises})
+
+
+def account(request, pk):
+    account_instance = Account.objects.get(id=pk)
+    routines = account_instance.routine_set.all()
+    routines_count = routines.count()
+    context = {
+        'routines': routines,
+        'account': account_instance,
+    }
+    return render(request, 'accounts/account.html', context)
