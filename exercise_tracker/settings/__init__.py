bd70a68162342161b63320110bef76af900ce353
Harika <h.duyu@rebuy.com>
2021-03-10 08:30:33 +0100

    update config

--- exercise_tracker/settings/__init__.py
+++ exercise_tracker/settings/__init__.py
@@ -3,8 +3,6 @@ import os
 
 environment = os.environ.get("EX_TRACKER_ENV")
 if environment != 'prod':
-    print("DEVELOPMENT ENVIRONMENT")
     from .dev import *
 else:
-    print("PRODUCTION")
     from .prod import *
--- exercise_tracker/settings/dev.py
+++ exercise_tracker/settings/dev.py
@@ -3,15 +3,3 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
-    }
-}
